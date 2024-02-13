54a194b31143a30869e9b527194b96277ca20763
KoshidaManato <k022g0129@g.neec.ac.jp>
2024-02-13 20:02:12 +0900

    プロジェクト ファイルを追加します。

---
+++ InputHandler.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ICommand.h"
+class InputHandler {
+public:
+	ICommand* HandleInput();
+
+	void AssignMoveLeftCommand2PreessKeyA();
+	void AssignMoveRightCommand2PreessKeyD();
+
+private:
+	ICommand* pressKeyA_;
+	ICommand* pressKeyD_;
+};
---
+++ Player.h
@@ -0,0 +1,17 @@
+﻿#include "Vector2.h"
+class Player {
+public:
+	Player();
+
+	void Init();
+	void Update();
+	void Draw();
+
+	// キーごとのコマンド
+	void MoveRight();
+	void MoveLeft();
+
+private:
+	Vector2 pos_;
+	float speed_;
+};
